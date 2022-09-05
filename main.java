abcd9d439cf5ed85c1e3ad6641dc225a51171c75
Bruno Mossmann <41453787+brunomossmann007@users.noreply.github.com>
2022-09-05 20:31:19 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+
+
+    public static void main(String[] args) {
+    
+        bubbleSort sort = new bubbleSort();
+        int[] a = new int[20];
+
+        sort.bubbleSort(a);
+}
+}
